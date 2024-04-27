0ef477c463d2314de2a2bfcf89b0457781a4d7db
einnad <146109295+einnad@users.noreply.github.com>
2024-04-27 17:38:11 +0100

    Switch transition for modal and add close modal functions

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+// elements
 const exButton = document.querySelectorAll(".ex");
+const closeModalButton = document.querySelector(".close-modal");
+
 const exModal = document.querySelector(".ex-modal");
 const overlay = document.querySelector(".overlay");
 
+// events
 exButton.forEach((button) => {
   button.addEventListener("click", () => {
-    exModal.classList.remove("hidden");
+    exModal.classList.add("ex-modal-open");
     overlay.classList.remove("hidden");
   });
 });
+
+// closing modal
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !exModal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+overlay.addEventListener("click", (e) => {
+  if (!exModal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+closeModalButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  exModal.classList.remove("ex-modal-open");
+  overlay.classList.add("hidden");
+}
